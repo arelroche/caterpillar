ab065462aa7b7980017acb051ca0b9f106b26a76
Girija <Girija@100.64.227.237>
2016-01-24 04:09:32 -0500

    merged recieving service changes

--- src/com/twilio/ReceivingService.java
+++ src/com/twilio/ReceivingService.java
@@ -1,47 +1,60 @@
 package com.twilio;
 
+
 import com.spotify.Database;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.twilio.sdk.*;
 import com.twilio.sdk.resource.instance.*;
 import com.twilio.sdk.resource.list.*;
 
+
 public class ReceivingService implements Runnable{
-	
+	// capitalize everything in hashmap
+	public static Map<String, Boolean> cellAdded_voted = new HashMap<String, Boolean>();
+
+
 	// Find your Account Sid and Token at twilio.com/user/account
 	public static final String ACCOUNT_SID = "AC09b34d194921b4d7fbdebce89575a6bb";
 	public static final String AUTH_TOKEN = "5945315ca88d07c9670c04173a2d96e5";
 
 	public void run() {
 		TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
-		
+
 		// Skip through old messages
 		int currMessages = 0;
 		MessageList messages = client.getAccount().getMessages();
 		for (Message message : messages) {
 			currMessages++;
 		}
-		
-		// Initialize the database
-		Database.init();
-		
-		
+
 		while (true) {
 			MessageList newMessages = client.getAccount().getMessages();
+			
 			for (Message message : newMessages);
+			
 			int numOfMessages = newMessages.getEnd() + 1;
 			if (numOfMessages > currMessages) {
 				for (Message message : newMessages) {
 					if (numOfMessages == currMessages)
 						break;
 					currMessages++;
-					
 					// Process messages here
-					System.out.println(message.getBody());	
+					System.out.println("from: " + message.getFrom() + " message: " + message.getBody());
+
+					if (cellAdded_voted.containsKey(message.getFrom()) && cellAdded_voted.containsValue(false)) {
+						// not allowed to vote more than once
+						//song choices being sent in
+						cellAdded_voted.put(message.getFrom(), true);
+					} else if (!cellAdded_voted.containsKey(message.getFrom())) {
+						cellAdded_voted.put(message.getFrom(), false);
+					} else { // case occurs when user tries to change their choice of song
+						continue;
+					}
 				}
 			}
-			
-			
-			
+
 			try {
 				Thread.sleep(5000);
 			} catch (InterruptedException e) {
