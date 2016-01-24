8378cc29f57e0c6cd85517e3255b91255cac3f4b
robrocop <rohit313.93@gmail.com>
2016-01-24 08:01:37 -0500

    Modified applescript again -Karan

--- src/com/spotify/SpotifyTerminal.java
+++ src/com/spotify/SpotifyTerminal.java
@@ -1,23 +1,13 @@
 package com.spotify;
 
-import com.music.ActiveSongs;
-import com.twilio.ReceivingService;
-
 public class SpotifyTerminal {
 	public static void play (String id) {
-		System.out.println("PLAYING SONG: " + id);
-		
+
 		try {
-			Thread.sleep(60000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+			Process pr = Runtime.getRuntime().exec("osascript SpotifyTest2.app/ " + id);
+			pr.waitFor();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-//		try {
-//			Process pr = Runtime.getRuntime().exec("osascript SpotifyTest2.app/ " + id);
-//			pr.waitFor();
-//		} catch (Exception e) {
-//
-//		}
 	}
 }
